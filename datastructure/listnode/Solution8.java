d4eecd0312b506f34d615a78f0a131a50d72f25d
Jovan L <327527689@qq.com>
2022-10-07 22:10:15 +0800

    added the solution8 for reversing the partition list nodes

---
+++ datastructure/listnode/Solution8.java
@@ -0,0 +1,32 @@
+package datastructure.listnode;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution8 {
+
+    @Test
+    void test() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        reverseRange(head.next, head.next.next.next);
+    }
+
+    ListNode reverseRange(ListNode a, ListNode b) {
+        ListNode previous = null;
+        ListNode current = a;
+        ListNode next = a;
+        while (current != b) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
