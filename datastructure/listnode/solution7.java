04c323dbac989976d9c073892b464302104cfcc5
Jovan L <327527689@qq.com>
2022-10-07 21:54:09 +0800

    added the solution7 for reversing the list node via iterator approch

---
+++ datastructure/listnode/solution7.java
@@ -0,0 +1,32 @@
+package datastructure.listnode;
+
+import org.junit.jupiter.api.Test;
+
+public class solution7 {
+
+    @Test
+    void test() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        traverse(head);
+    }
+
+    ListNode traverse(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = head;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
