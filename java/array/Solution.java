5fdfaf4c1346b9dd70cd1c9d74c79a4f20ece013
Jovan L <327527689@qq.com>
2022-10-07 15:02:13 +0800

    first commit

---
+++ java/array/Solution.java
@@ -0,0 +1,18 @@
+package java.array;
+
+import org.junit.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        int[] arr = new int[] {1, 2, 3};
+        traverse(arr);
+    }
+
+    void traverse(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " -> ");
+        }
+    }
+}
